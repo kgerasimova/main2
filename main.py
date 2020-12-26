f4bee6ecd6089bbc2b8c4ce0f702c96e47317471
kgerasimova <74534535+kgerasimova@users.noreply.github.com>
2020-12-26 11:23:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-def solution(lt=list):
-    answer=False
-    element=lt[0]
-    lst=lt[1::]
-    for i in lst:
-        if element<=i:
-            answer=True
-        else:
-            answer=False
-            break
-        element=i
-    if answer:
-        return answer
-    else:
-        x=False
-        element=lt[0]
-        for i in lst:
-            if element>=i:
-                answer=True
-            else:
-                answer=False
-                break
-            element=i
-        if answer:
-            return answer
-        else: return  answer
-
 def solution(a=list):
     n=0
     mis=0
